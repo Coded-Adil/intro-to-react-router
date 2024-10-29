dc585c2d75e19858a1a245d257c66e1c74b34ba1
Coded-Adil <itsadil2022@gmail.com>
2024-10-29 14:29:24 +0600

    revisied previous learnings and learned about link tag as well

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import "./Home.css"
+import Footer from "../Footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-            <h2>Welcome Home</h2>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,10 @@
+const Users = () => {
+    return (
+        <div>
+            <h2>Our Users</h2>
+            <p>You fooking good user.</p>            
+        </div>
+    );
+};
+
+export default Users;
